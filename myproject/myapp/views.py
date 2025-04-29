e8c6adb2f676c925fae5ff3451f34a38998181a5
KristiyanAbrashkov <162896853+KristiyanAbrashkov@users.noreply.github.com>
2025-04-29 20:33:00 +0300

    Работа с templates и basic създаване на reviews и home
    Създадох начална версия на някои от страниците, с цел създаване на основа, която да се надгради и подобри.

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+review = [
+    {'id':1, 'name': 'GOOD'},
+    {'id':2, 'name': 'MID'},
+    {'id':3, 'name': 'BAD'},
+]
+def reviews(request):
+    context = {'review': review}
+    return render(request, 'myapp/review.html', context)
+
+def home(request):
+    return render(request, 'myapp/home.html')
