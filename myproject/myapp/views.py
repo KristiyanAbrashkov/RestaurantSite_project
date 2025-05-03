d57e5f096a4f86069cd245effbd7f97110bcfdd7
KristiyanAbrashkov <162896853+KristiyanAbrashkov@users.noreply.github.com>
2025-05-03 13:26:48 +0300

    Промяна на имена

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 review = [
-    {'id':1, 'name': 'GOOD'},
-    {'id':2, 'name': 'MID'},
-    {'id':3, 'name': 'BAD'},
+    {'review': 'GOOD'},
+    {'review': 'MID'},
+    {'review': 'BAD'},
 ]
 def reviews(request):
     context = {'review': review}
-    return render(request, 'myapp/review.html', context)
+    return render(request, 'myapp/reviews.html', context)
 
 def home(request):
     return render(request, 'myapp/home.html')
